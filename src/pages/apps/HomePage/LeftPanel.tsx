47fd675e0d7a66483542a9e2c35642835ed3912e
Nguyen Hoang <metmoilamroiday4@gmail.com>
2024-08-29 11:04:38 +0700

    home page font
    change font style

--- src/pages/apps/HomePage/LeftPanel.tsx
+++ src/pages/apps/HomePage/LeftPanel.tsx
@@ -5,12 +5,12 @@ const LeftSide = () => {
 	return (
 		<div className="card p-6 h-3/6 min-h-1/6 lg:rounded-md rounded-none">
 			<div data-simplebar className="p-6">
-			<h3 className="text-lg uppercase">Các danh mục</h3>
+			<h2 className="text-lg uppercase">Các danh mục</h2>
 				<div className="flex flex-col">
 					{categories.map((category, idx) => (
-						<div key={idx} className="flex items-center gap-2 mt-1.5">
+						<div key={idx} className="flex items-center gap-4 mt-1.5">
 							<input type="checkbox" className="form-checkbox rounded" id={`checklist-${idx}`}/>
-							<label className="font-bold text-gray-500 dark:text-gray-300 text-sm" htmlFor={`checklist-${idx}`}>
+							<label className="font-semibold text-reset" htmlFor={`checklist-${idx}`}>
 								{category}
 							</label>
 						</div>
